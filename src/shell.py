394ee6c682f666ab58ffdd094aa20eaa47e8d8a9
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-02-07 12:23:19 -0500

    Update shell.py

--- src/shell.py
+++ src/shell.py
@@ -19,9 +19,9 @@ def shell():
             exit()
         elif command.startswith('nscan'):
             if len(command.split()) < 2:
-                PortScanner.HelpMenu()
+                Tools.PortScanner.HelpMenu()
             elif command.split()[1] == '--help':
-                PortScanner.HelpMenu()
+                Tools.PortScanner.HelpMenu()
             else:
                 host = command.split()[1]
                 try:
@@ -34,96 +34,96 @@ def shell():
                     ports = None
                 except ValueError:
                     print("Ports must be numbers.")
-                    PortScanner.HelpMenu()
+                    Tools.PortScanner.HelpMenu()
                 if ports:
-                    PortScanner.scan_host(host, ports)
+                    Tools.PortScanner.scan_host(host, ports)
                 else:
-                    PortScanner.scan_ports(host)
+                    Tools.PortScanner.scan_ports(host)
         elif command.startswith('whois'):
                 if len(command.split()) < 2:
-                    WhoisLookup.HelpMenu()
+                    Tools.WhoisLookup.HelpMenu()
                 elif command.split()[1] == '--help':
-                    WhoisLookup.HelpMenu()
+                    Tools.WhoisLookup.HelpMenu()
                 else:
                     domain_or_ip = command.split()[1]
-                    WhoisLookup.whois_lookup(domain_or_ip)
+                    Tools.WhoisLookup.whois_lookup(domain_or_ip)
         elif command.startswith('hashident'):
             if len(command.split()) < 2:
-                Hashid.HelpMenu()
+                Tools.Hashid.HelpMenu()
             elif command.split()[1] == '--help':
-                Hashid.HelpMenu()
+                Tools.Hashid.HelpMenu()
             else:
                 hash_input = command.split()[1]
-                print(Hashid.hash_identifier(hash_input))
+                print(Tools.Hashid.hash_identifier(hash_input))
         elif command == 'uname -f':
-            Defaults.Uname()
+            System.Uname()
         elif command.startswith('IP'):
             if len(command.split()) < 2:
-                IPLookup.HelpMenu()
+                Tools.IPLookup.HelpMenu()
             elif command.split()[1] == '--help':
-                IPLookup.HelpMenu()
+                Tools.IPLookup.HelpMenu()
             else:
                 ip_address = command.split()[1]
-                data = IPLookup.ip_lookup(ip_address)
+                data = Tools.IPLookup.ip_lookup(ip_address)
                 print(f"Location: {data['city']}, {data['region']}, {data['country']}")
                 print(f"ISP: {data['org']}")
                 print(f"Location: {data['loc']}")
                 print(f"Postal Code: {data['postal']}")
         elif command.startswith('Subnet'):
             if len(command.split()) < 2:
-                SubnetCalculator.HelpMenu()
+                Tools.SubnetCalculator.HelpMenu()
             elif command.split()[1] == '--help':
-                SubnetCalculator.HelpMenu()
+                Tools.SubnetCalculator.HelpMenu()
             else:
                 ip_address = command.split()[1]
                 subnet_mask = command.split()[2]
-                print(SubnetCalculator.subnet_calculator(ip_address, subnet_mask))
+                print(Tools.SubnetCalculator.subnet_calculator(ip_address, subnet_mask))
         elif command == 'passgen':
-            print('Password: ' + Defaults.generate_password(16))
+            print('Password: ' + System.generate_password(16))
         elif command.startswith('URLcheck'):
             if len(command.split()) < 2:
-                URLcheck.HelpMenu()
+                Tools.URLcheck.HelpMenu()
             elif command.split()[1] == '--help':
-                URLcheck.HelpMenu()
+                Tools.URLcheck.HelpMenu()
             else:
                 url = command.split()[1]
-                URLcheck.url_checker(url)
+                Tools.URLcheck.url_checker(url)
         elif command == 'help':
-            HelpMenu.MainHelp()
+            System.HelpMenu.MainHelp()
         elif command.startswith('DNS'):
             if len(command.split()) < 2:
-                DNSlookup.HelpMenu()
+                Tools.DNSlookup.HelpMenu()
             elif command.split()[1] == '--help':
-                DNSlookup.HelpMenu()
+                Tools.DNSlookup.HelpMenu()
             else:
                 url = command.split()[1]
-                DNSlookup.DNSlookup(url)
+                Tools.DNSlookup.DNSlookup(url)
         elif command.startswith('buster'):
             if len(command.split()) < 2:
-                DirBuster.HelpMenu()
+                Tools.DirBuster.HelpMenu()
             elif command.split()[1] == '--help':
-                DirBuster.HelpMenu()
+                Tools.DirBuster.HelpMenu()
             else:
                 wordlist = 'wordlist.txt'
                 url = command.split()[1]
-                DirBuster.directory_buster(url, wordlist)
+                Tools.DirBuster.directory_buster(url, wordlist)
         elif command.startswith('hash'):
             if len(command.split()) < 2:
-                Hashes.HelpMenu()
+                Tools.Hashes.HelpMenu()
             elif command.split()[1] == '--help':
-                Hashes.HelpMenu()
+                Tools.Hashes.HelpMenu()
             else:
                 string = command.split()[1]
                 algorithm = command.split()[2]
-                print(Hashes.hash_string(string, algorithm))
+                print(Tools.Hashes.hash_string(string, algorithm))
         elif command.startswith('URLshort'):
             if len(command.split()) < 2:
-                URLshortener.HelpMenu()
+                Tools.URLshortener.HelpMenu()
             elif command.split()[1] == '--help':
-                URLshortener.HelpMenu()
+                Tools.URLshortener.HelpMenu()
             else:
                 long_url = command.split()[1]
-                url_shortener = URLshortener()
+                url_shortener = Tools.URLshortener()
                 short_url = url_shortener.shorten_url(long_url)
                 print(f"Short URL: {short_url}")
                 print(f"Redirect URL: {url_shortener.redirect_url(short_url)}")
